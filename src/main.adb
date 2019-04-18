51e1af6cdf104d3f046f984ebfa488170b09e545
psyomn <lethaljellybean@gmail.com>
2019-04-17 23:48:07 -0400

    Use proper 3 space Ada indentation

--- src/main.adb
+++ src/main.adb
@@ -7,42 +7,42 @@ with GnatGen;
 -- Entry point for this command line application.
 procedure Main is
 
-  package US renames Ada.Strings.Unbounded;
+   package US renames Ada.Strings.Unbounded;
 
-  Parameters : GnatGen.String_Array(1..Argument_Count);
+   Parameters : GnatGen.String_Array(1..Argument_Count);
 
-  procedure Print_Usage is
-  begin
-    Put_Line("Use:");
-    Put_Line("gnatgen <action> [params]");
-    Put_Line("  new project - to create a template of a new project");
-  end Print_Usage;
+   procedure Print_Usage is
+   begin
+      Put_Line("Use:");
+      Put_Line("gnatgen <action> [params]");
+      Put_Line("  new project - to create a template of a new project");
+   end Print_Usage;
 
 begin
 
-  if Argument_Count < 1 then
-    Print_Usage;
-    return;
-  end if;
+   if Argument_Count < 1 then
+      Print_Usage;
+      return;
+   end if;
 
-  -- Copy parameters
-  for ix in 1..Argument_Count loop
-    Parameters(ix) := US.To_Unbounded_String(Argument(Number => ix));
-  end loop;
+   -- Copy parameters
+   for ix in 1..Argument_Count loop
+      Parameters(ix) := US.To_Unbounded_String(Argument(Number => ix));
+   end loop;
 
-  -- Add here any other possible commands
-  if Argument(Number => 1) = "new" then
-    GnatGen.handle_new(Parameters);
+   -- Add here any other possible commands
+   if Argument(Number => 1) = "new" then
+      GnatGen.handle_new(Parameters);
 
-  elsif Argument (Number => 1) = "throwaway" or
-        Argument (Number => 1) = "t" then
-    GnatGen.Handle_Throwaway;
+   elsif Argument (Number => 1) = "throwaway" or
+     Argument (Number => 1) = "t" then
+      GnatGen.Handle_Throwaway;
 
-  elsif Argument(Number => 1) = "print" or
-        Argument(Number => 1) = "p" then
-    GnatGen.Handle_Print(Parameters);
+   elsif Argument(Number => 1) = "print" or
+     Argument(Number => 1) = "p" then
+      GnatGen.Handle_Print(Parameters);
 
-  else
-    Put_Line("Did not understand that");
-  end if;
+   else
+      Put_Line("Did not understand that");
+   end if;
 end Main;
