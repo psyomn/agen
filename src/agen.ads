c36020981b7ad8e19312d5a60457a2add427def3
Patrick Kelly <p-kell@live.com>
2019-04-24 14:04:59 -0400

    Added Stub_Comments parameter to all function and procedure generators

--- src/agen.ads
+++ src/agen.ads
@@ -1,17 +1,3 @@
--- Copyright 2014-2019 Simon Symeonidis (psyomn)
---
--- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at
---
---   http://www.apache.org/licenses/LICENSE-2.0
---
--- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS,
--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- See the License for the specific language governing permissions and
--- limitations under the License.
-
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
 package Agen is
@@ -54,21 +40,39 @@ package Agen is
 
   procedure Print_Procedure(Name : String);
 
+  procedure Print_Procedure(Name : String; Stub_Comments : Boolean);
+
   procedure Print_Procedure(Name : String; Param : Parameter);
+
+  procedure Print_Procedure(Name : String; Param : Parameter; Stub_Comments : Boolean);
 
   procedure Print_Procedure(Name : String; Params : Parameter_Array);
 
+  procedure Print_Procedure(Name : String; Params : Parameter_Array; Stub_Comments : Boolean);
+
   procedure Print_Function(Form : Parameter);
+
+  procedure Print_Function(Form : Parameter; Stub_Comments : Boolean);
 
   procedure Print_Function(Name : String; Returns : String);
 
+  procedure Print_Function(Name : String; Returns : String; Stub_Comments : Boolean);
+
   procedure Print_Function(Form : Parameter; Param : Parameter);
+
+  procedure Print_Function(Form : Parameter; Param : Parameter; Stub_Comments : Boolean);
 
   procedure Print_Function(Name : String; Returns : String; Param : Parameter);
 
+  procedure Print_Function(Name : String; Returns : String; Param : Parameter; Stub_Comments : Boolean);
+
   procedure Print_Function(Form : Parameter; Params : Parameter_Array);
 
+  procedure Print_Function(Form : Parameter; Params : Parameter_Array; Stub_Comments : Boolean);
+
   procedure Print_Function(Name : String; Returns : String; Params : Parameter_Array);
+
+  procedure Print_Function(Name : String; Returns : String; Params : Parameter_Array; Stub_Comments : Boolean);
 
 private
   type Parameter is record
